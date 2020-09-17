f9ec8c0d6534d32abc6bc08d7f041657cd32d566
Afonso Raposo <afonsocraposo@gmail.com>
2020-09-17 01:04:31 +0100

    Updates

--- window-title.sh
+++ window-title.sh
@@ -2,7 +2,7 @@
 PREV=""
 NEW=""
 while true; do
-    NEW=$(xdotool getactivewindow getwindowname | cut -c -64)
+    NEW=$(xdotool getactivewindow getwindowname | cut -c -64 | iconv -f utf-8 -t utf-8 -c)
     if [ "$PREV" != "$NEW" ]
     then
         echo $NEW
